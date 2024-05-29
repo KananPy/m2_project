ae3ed68fed4895b06fbae0eec3d89e5ca95e6725
KananPy <kenan.semenderli@gmail.com>
2024-05-29 10:49:00 +0400

    react app

---
+++ src/components/CardContainer/CardContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+function CardContainer() {
+  return (
+    <div>
+        <Card/>
+    </div>
+  )
+}
+
+export default CardContainer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../NavBar/Navbar'
+
+function Header() {
+  return (
+    <>
+       <Navbar/>
+
+    </>
+  )
+}
+
+export default Header
